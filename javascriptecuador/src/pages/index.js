bad2c89891d52c2f0a6ef8e045415b956216c29c
Richard Palacios <apalaciosg91@gmail.com>
2019-12-23 06:24:46 -0500

    Inicio sitio javascriptecuador con gatsby

---
+++ javascriptecuador/src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hola</h1>
+    <p>Bienvenido al sitio de la <span style={{ color: `yellow`, fontSize: `40px` }}>Comunidad</span> <span style={{ color: `blue`, fontSize: `40px` }}>Javascript</span><span style={{ color: `red`, fontSize: `40px` }}>Ecuador</span>.</p>
+    <p>Este sitio está en desarrollo pero si quieres ayudar pasa por:.</p>
+    <a href="https://github.com/javascriptecuador/javascriptecuador.github.com">Nuestro repositorio en github</a>    
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />      
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
