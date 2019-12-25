09a5bcc1900e38aa8ac4fdff2d52fb6b7a60bc03
Richard Palacios <apalaciosg91@gmail.com>
2019-12-25 13:08:36 -0500

    Agrego url's de telegram y twitter

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,16 @@ const IndexPage = () => (
     <h1>Hola</h1>
     <p>Bienvenido al sitio de la <span style={{ color: `yellow`, fontSize: `40px` }}>Comunidad</span> <span style={{ color: `blue`, fontSize: `40px` }}>Javascript</span><span style={{ color: `red`, fontSize: `40px` }}>Ecuador</span>.</p>
     <p>Este sitio está en desarrollo pero si quieres ayudar pasa por: <a href="https://github.com/javascriptecuador/javascriptecuador.github.com">Nuestro repositorio en github</a></p>    
+    <h2>Pasa a saludar a:</h2>
+    <ul>
+      <li>Grupo en Telegram: <a href="https://t.me/javascriptecuador">JavaScript Ecuador</a> </li>    
+    </ul>
+    <h2>También estamos en:</h2>
+    <ul>
+      <li>Twitter: <span><a href="https://twitter.com/jsecuador_">@jsecuador_</a> </span> </li>
+      <li>Facebook</li>
+      <li>Instagram</li>
+    </ul>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />      
     </div>
