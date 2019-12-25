fd00d384873bf900104088553fdd8a6420456fd6
Richard Palacios <apalaciosg91@gmail.com>
2019-12-25 14:16:48 -0500

    Update page index, add emojis

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,28 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hola</h1>
-    <p>Bienvenido al sitio de la <span style={{ color: `yellow`, fontSize: `40px` }}>Comunidad</span> <span style={{ color: `blue`, fontSize: `40px` }}>Javascript</span><span style={{ color: `red`, fontSize: `40px` }}>Ecuador</span>.</p>
-    <p>Este sitio está en desarrollo pero si quieres ayudar pasa por: <a href="https://github.com/javascriptecuador/javascriptecuador.github.com">Nuestro repositorio en github</a></p>    
-    <h2>Pasa a saludar a:</h2>
+    <p>✨ Bienvenido al sitio de la <span style={{ color: `yellow`, fontSize: `16px` }}>Comunidad</span> <span style={{ color: `blue`, fontSize: `16px` }}> Javascript</span><span style={{ color: `red`, fontSize: `16px` }}> Ecuador</span> .</p>
+    <p>Actualmente este sitio está en desarrollo, estamos buscando ayuda si quieres apoyar pasa por <a href="https://github.com/javascriptecuador/javascriptecuador.github.com">Nuestro repositorio en github</a></p>    
+    <h2>👋 Pasa a saludar a:</h2>
     <ul>
       <li>Grupo en Telegram: <a href="https://t.me/javascriptecuador">JavaScript Ecuador</a> </li>    
     </ul>
-    <h2>También estamos en:</h2>
+    <h2>💫 También estamos en:</h2>
     <ul>
       <li>Twitter: <span><a href="https://twitter.com/jsecuador_">@jsecuador_</a> </span> </li>
       <li>Facebook</li>
       <li>Instagram</li>
     </ul>
+    <h2>👀 Código de Conducta:</h2>
+    <p>
+    Tenemos un código de conducta puedes leerlo <a href="https://github.com/javascriptecuador/.github/blob/master/CODE_OF_CONDUCT.md">aquí</a> .
+    </p>
+    
+    <h2>🚀 Proponer una charla</h2>
+    <p>
+    Tienes algo que contar o compartir, crea un issue proponiendo el tema <a href="https://github.com/javascriptecuador/Meetups">aquí</a> .
+    </p>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />      
     </div>
