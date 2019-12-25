0654dfe79bc55693f37891e3cc7fe4393e04970d
Richard Palacios G <apalaciosg91@gmail.com>
2019-12-25 16:50:18 -0500

    Update gatsby-config.js
    Modify description site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Javascript Ecuador`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Sitio de la comunidad Javascript Ecuador #Javascript #community.`,
     author: `@gatsbyjs`,
   },
   plugins: [
